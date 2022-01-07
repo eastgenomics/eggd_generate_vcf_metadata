507247c16a83a07e21560709e7c6b17bdd00ab5c
jethror1 <rainford1995@gmail.com>
2022-01-07 20:59:42 +0200

    fix getting filename prefix

--- src/eggd_generate_vcf_metadata.sh
+++ src/eggd_generate_vcf_metadata.sh
@@ -8,16 +8,17 @@ main() {
     echo "Value of vcf: '$vcf'"
 
     # split vcf filename parts to an array
-    IFS='-' read -a arr <<< $vcf_name
+    IFS='-' read -a arr <<< "$vcf_name"
+
+    # _prefix won't work, get prefix ourselves
+    IFS='.' read -r vcf_prefix _ <<< "$vcf_name"
+    json="${vcf_prefix}.metadata.json"
 
     # create json from file name parts, formatted from haemonc file naming
-    json="$vcf_prefix-metadata.json"
-
     printf '''{
         "Filename": "%s", "Individual ID":"%s", "Sample ID": "%s",
         "Sex": "%s", "Panel": "all haem onc", "Disorder": "HaemOnc", "CaseID": "%s"
-    }''' "${file}" "${arr[0]}" "${arr[1]}" "${arr[5]}" "H${arr[0]}_1" | jq '.' > "$json"
-
+    }''' "${vcf_name}" "${arr[0]}" "${arr[1]}" "${arr[5]}" "H${arr[0]}_1" | jq '.' > "$json"
 
     json=$(dx upload "$json" --brief)
     dx-jobutil-add-output json "$json" --class=file
